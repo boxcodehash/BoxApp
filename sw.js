e2ab1027d3a33e248e64515c9750e36db346529f
boxcodehash <boxtvpr1@gmail.com>
2026-01-30 20:32:41 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = 'muzz-v3';
+const CACHE_NAME = 'muzz-cache-v1';
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
+// Archivos a guardar para que funcione offline
+const assets = [
+  "./index.html",
+  "./chat.html",
+  "./swap.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        'index.html',
-        'login.html',
-        'chat.html',
-        'swap.html',
-        'manifest.json'
-      ]).catch(err => console.log("Error caching:", err));
+      return cache.addAll(assets);
     })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+// Este evento es el que activa el botón de "Instalar" en Android
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
