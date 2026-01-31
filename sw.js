09cf59b41b4bbefe35db5985885d5c1c1891e28e
boxcodehash <boxtvpr1@gmail.com>
2026-01-30 20:19:38 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
-const CACHE_NAME = 'muzzsnap-v1';
-const ASSETS = [
-  'login.html',
-  // Agrega aquí 'chat.html' y tus CSS/JS locales si los tienes
+const CACHE_NAME = 'muzz-v2';
+const assets = [
+  '/MuzzSnap/',
+  '/MuzzSnap/index.html',
+  '/MuzzSnap/login.html',
+  '/MuzzSnap/chat.html',
+  '/MuzzSnap/swap.html',
+  '/MuzzSnap/manifest.json'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
